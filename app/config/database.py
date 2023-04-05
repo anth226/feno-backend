336fb7ac74ea5dea9ff439841658a6bd02b4c8c7
endlessorhub <anthonyhutchi99@gmail.com>
2023-04-05 23:36:18 +0500

    db uri updated

--- app/config/database.py
+++ app/config/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://root:root@localhost:5432/fenodb"
+SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost:5432/fenodb"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
