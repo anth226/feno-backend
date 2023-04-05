0dd644358d6b3a94a4993e2128e1b91eaa28a04b
endlessorhub <anthonyhutchi99@gmail.com>
2023-04-06 00:49:04 +0500

    Add new user table to database and done migration

---
+++ alembic/versions/49f08717ff07_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 49f08717ff07
+Revises: 
+Create Date: 2023-04-06 00:46:25.127270
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '49f08717ff07'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
