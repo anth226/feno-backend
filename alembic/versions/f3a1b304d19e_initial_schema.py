77f3331a37ff827b69d983435d2eb9851154ad98
endlessorhub <anthonyhutchi99@gmail.com>
2023-04-14 23:15:40 +0500

    target_metadata updated

---
+++ alembic/versions/f3a1b304d19e_initial_schema.py
@@ -0,0 +1,24 @@
+"""Initial schema
+
+Revision ID: f3a1b304d19e
+Revises: 49f08717ff07
+Create Date: 2023-04-14 22:53:58.868432
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f3a1b304d19e'
+down_revision = '49f08717ff07'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
